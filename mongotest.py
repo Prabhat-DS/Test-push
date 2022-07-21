da1517ebabe9b7fcdfb40657a38ec66ad70e7cc9
Prabhat-DS <prabhatkumar.18811@gmai>
2022-07-21 15:42:08 +0530

    this is my step to push code into Github

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://Prabhat:pr171219#@prabhat-cluster0.szwyq.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d = {
+    "name":"prabhat1",
+    "email" : "prabhat1@gmail.com",
+    "surname" : "kumar"
+}
+db1 = client['mongotest1']
+coll = db1['test']
+# coll.insert_one(d)
